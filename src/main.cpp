1cf0e3ec58770b772c1d8603ee139eb83bf91874
faaip <frederik@tollund.cc>
2020-09-24 16:10:38 +0200

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+int JOYX_PIN = 39;
+int JOYY_PIN = 36;
+int JOYBTN_PIN = 34;
+
+int lcdColumns = 16;
+int lcdRows = 2;
+
+// set LCD address, number of columns and rows
+// if you don't know your display address, run an I2C scanner sketch
+LiquidCrystal_I2C lcd(0x3F, lcdColumns, lcdRows);
+
+void setup() {
+  Serial.begin(9600);
+
+  // initialize LCD
+  lcd.init();
+  // turn on LCD backlight
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+}
+
+bool isInDeadZone(int joyX, int joyY) {
+  int joyXCenterVal = 1715;
+  int joyYCenterVal = 1776;
+  int offset = 150;
+  bool deadX = joyX < joyXCenterVal + offset && joyX > joyXCenterVal - offset;
+  bool deadY = joyY < joyYCenterVal + offset && joyY > joyYCenterVal - offset;
+  return deadX && deadY;
+}
+
+void formatPrintJoystickValues(std::pair<int,int> joystick) {
+  String s = "X: " + String(joystick.first) + ", Y: " + String(joystick.second);
+  Serial.println(s);
+}
+
+std::pair<int, int> getJoystickValues() {
+  int xVal = analogRead(JOYX_PIN);
+  int yVal = analogRead(JOYY_PIN);
+  return std::make_pair(xVal, yVal);
+}
+
+void loop() {
+  std::pair<int, int> joystick = getJoystickValues();
+  bool isMoving = isInDeadZone(joystick.first, joystick.second) == false;
+  if (isMoving) {
+    formatPrintJoystickValues(joystick);
+    String s = "X: " + String(joystick.first) + ", Y: " + String(joystick.second);
+    lcd.print(s);
+  } else {
+    lcd.print("Not moving");
+  }
+  lcd.clear();
+}
